c28fc13e9808747f809189d841b59d3b33399601
Jack <jack@stet.com>
2017-08-15 21:53:58 -0400

    working error validation for form, added styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,10 +6,15 @@ class Login extends Component {
     super();
     this.state = {
       mode: 'login',
-      showErrors: false,
-      validationErrors: {},
+      validationErrors: {
+        user: { taken: false, inval: true },
+        pass: { inval: true },
+        confirm: { inval: true }
+      },
       user: null,
-      pass: null
+      pass: null,
+      confirm: null,
+      invalid: true
     };
   }
 
@@ -48,16 +53,82 @@ class Login extends Component {
       },
       body: JSON.stringify(payload)
     })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson);
+    });
   }
 
-  getErrors() {
-
+  checkValid(name,value) {
+    this.setState({
+      invalid: (this.state.validationErrors.user.taken ||
+        this.state.validationErrors.user.inval ||
+        this.state.validationErrors.pass.inval ||
+        this.state.validationErrors.confirm.inval)
+    });
   }
 
   handleInputChange(event) {
     const target = event.target;
     const value = target.value;
     const name = target.name;
+
+    var userspec = new RegExp("^[a-zA-Z0-9]([._](?![._])|[a-zA-Z0-9]){4,18}[a-zA-Z0-9]$");
+    var passspec = new RegExp("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$");
+
+    if(name === 'user'){
+       if(!userspec.test(value)) {
+        this.setState({
+          validationErrors: {
+            ...this.state.validationErrors,
+            user: { ...this.state.validationErrors.user, inval: true }
+          }
+        }, () => this.checkValid());
+      } else {
+        this.setState({
+          validationErrors: {
+            ...this.state.validationErrors,
+            user: { ...this.state.validationErrors.user, inval: false }
+          }
+        }, () => this.checkValid());
+      }
+    }
+
+    if(name === 'pass'){
+       if(!passspec.test(value)) {
+        this.setState({
+          validationErrors: {
+            ...this.state.validationErrors,
+            pass: { inval: true }
+          }
+        }, () => this.checkValid());
+      } else {
+        this.setState({
+          validationErrors: {
+            ...this.state.validationErrors,
+            pass: { inval: false }
+          }
+        }, () => this.checkValid());
+      }
+    }
+
+    if(name === 'confirm'){
+       if(value !== this.state.pass) {
+        this.setState({
+          validationErrors: {
+            ...this.state.validationErrors,
+            confirm: { inval: true }
+          }
+        }, () => this.checkValid());
+      } else {
+        this.setState({
+          validationErrors: {
+            ...this.state.validationErrors,
+            confirm: { inval: false }
+          }
+        }, () => this.checkValid());
+      }
+    }
 
     this.setState({
       [name]: value
@@ -73,12 +144,12 @@ class Login extends Component {
           </Modal.Header>
           <Modal.Body>
             <form className="flex-column flex-center">
-              <label>
-                Username:
+              <label className="pv-5">
+                <span className="prm">Username:</span>
                 <input type="text" name="user" onChange={this.handleInputChange.bind(this)}/>
               </label>
-              <label>
-                Password:
+              <label className="pv-5">
+                <span className="prm">Password:</span>
                 <input type="text" name="pass" onChange={this.handleInputChange.bind(this)}/>
               </label>
             </form>
@@ -90,6 +161,11 @@ class Login extends Component {
         </div>
       );
     } else if(this.state.mode === 'signup'){
+      /*
+        TODO:
+        - add visible error messages
+        - get 'taken' error from server and display message
+      */
       return(
         <div>
           <Modal.Header closeButton>
@@ -97,19 +173,23 @@ class Login extends Component {
           </Modal.Header>
           <Modal.Body>
             <form className="flex-column flex-center">
-              <label>
-                Username:
+              <label className="pv-5">
+                <span className="prm">Username:</span>
                 <input type="text" name="user" onChange={this.handleInputChange.bind(this)}/>
               </label>
-              <label>
-                Password:
+              <label className="pv-5">
+                <span className="prm">Password:</span>
                 <input type="text" name="pass" onChange={this.handleInputChange.bind(this)}/>
               </label>
+              <label className="pv-5">
+                <span className="prm">Retype Password:</span>
+                <input type="text" name="confirm" onChange={this.handleInputChange.bind(this)}/>
+              </label>
             </form>
-            <h5>If you already have an account, <a onClick={() => this.setState({ mode: 'login'})}>click here</a></h5>
           </Modal.Body>
           <Modal.Footer>
-            <Button onClick={() => this.signUp()}>Submit</Button>
+            <Button onClick={() => this.setState({ mode: 'login'})}>Login</Button>
+            <Button onClick={() => this.signUp()} disabled={this.state.invalid}>Submit</Button>
           </Modal.Footer>
         </div>
       );
