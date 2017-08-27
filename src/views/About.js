5f12f22e19a3d9cefd4f6b8420dea328873f5450
Jack <jack@stet.com>
2017-08-27 01:56:07 -0400

    copy changes and minor styling

--- src/views/About.js
+++ src/views/About.js
@@ -15,14 +15,14 @@ const About = () => (
       </h4>
       <h2 className="deeper-sea">Security</h2>
       <h4 className="text-color">
-        This site stores encrypted passwords, so even the site admins don't have access to user information.
+        This site stores hashed passwords, so even the site admins don't have access to user information.
         <br/>
         <br/>
         In addition, when a program link is shared, the program is not served to the client. Instead, the program is executed with user parameters in a sandbox on the server.
         This means any private data in the program isn't distributed to program users.
         <br/>
         <br/>
-        <span className="light-sea">Despite these security measures,</span> this site still shouldn't be used for extremely sensitive information in the event of a compromise.
+        Despite these security measures, this site still shouldn't be used for extremely sensitive information in the event of a compromise.
         <br/>
         <br/>
         We are <span className="light-sea">NOT RESPONSIBLE</span> for any user data exposed.
