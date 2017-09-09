00bf8dea5dd9787790b5b94e1750751b83a59f9f
Jack <jack@stet.com>
2017-09-09 16:48:09 -0400

    copy change

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -17,7 +17,7 @@ class Landing extends Component {
           <h1 className="mbn">Welcome to <span className="light-sea">BlckBx!</span></h1>
           <h2 className="mts mbm">Your programs, <span className="deep-sea">anywhere.</span></h2>
           <h4>If you've ever wanted a quick and easy way to share a program with a colleague or friend without the hassle of building a GUI, then BlckBx is the tool for you.</h4>
-          <h4>Now, the process of distributing text based programs is streamlined. Head on over to the <Link to="/instructions" className="deep-sea">Help</Link> section for more info and program ideas.</h4>
+          <h4>The process of distributing text based programs has been streamlined. Head on over to the <Link to="/instructions" className="deep-sea">Help</Link> section for more info and program ideas.</h4>
           <h4>To get started, signup below.</h4>
           <Button className="btn-primary mtm" onClick={() => this.props.updateLogin()}>
             Signup
